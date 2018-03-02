5504455f9cd7b78f80ab2cef649341c32993e60a
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-02 18:53:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,22 +11,16 @@
 def key_for_min_value(name_hash)
 min_value_key = ""
 values = []
-min_value = values[1]
-i = 3
+min_value = 0
 
   name_hash.each do |k, v|
-  values.push(k,v)
+  values.push(v)
   # could maybe use name_hash.to_a
   end
 
-  while i < values.length do
-    if values[i] < min_value
-      min_value_key << values[i - 1]
-    else
-      min_value_key << values[0]
-    i += 2
+  values.sort 
+  end
+
+values
 end
 
-min_value_key
-end
-end
