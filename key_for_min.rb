9284f4e33cab31638cec33f94d7a57ca11413dae
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-02 19:57:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,7 @@
 def key_for_min_value(name_hash)
 values = []
 hash_array = name_hash.to_a
-
+min_key = ""
 
   name_hash.each do |k, v|
   values.push(v)
@@ -22,7 +22,7 @@ hash_array = name_hash.to_a
 
 while i < hash_array.length do
   if hash_array[i][1] == min_value[0]
-    puts hash_array[i][0]
+    min_key << hash_array[i][0]
   end
     i += 1
 end
